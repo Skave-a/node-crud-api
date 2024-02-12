7a1e9883455f6dca9f71b770afe8ca81c3b71e75
Skave-a <arrowtata@gmail.com>
2024-02-12 21:47:45 +0300

    fix: router

--- src/index.ts
+++ src/index.ts
@@ -1,120 +1,24 @@
 import dotenv from 'dotenv';
 import { IncomingMessage, ServerResponse, createServer } from 'node:http';
-import { v4 as uuidv4, validate as validateUuid } from 'uuid';
+import { handleGetUsers, handleGetUser, handleCreateUser, handleUpdateUser, handleDeleteUser } from './router/Router';
 
 dotenv.config();
 
 // Constants
 const PORT = process.env.PORT || 3001;
 
-// Placeholder data for users
-const users: { id: string; username: string; age: number; hobbies: string[] }[] = [];
-
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   try {
     if (req.url === '/api/users' && req.method === 'GET') {
-      res.setHeader('Content-Type', 'application/json');
-      res.statusCode = 200;
-      res.end(JSON.stringify(users));
+      handleGetUsers(req, res);
     } else if (req.url?.startsWith('/api/users/') && req.method === 'GET') {
-      const userId = req.url.split('/')[3];
-  
-      if (!validateUuid(userId)) {
-        res.setHeader('Content-Type', 'application/json');
-        res.statusCode = 400;
-        res.end(JSON.stringify({ message: 'Invalid userId' }));
-      } else {
-        const user = users.find((u) => u.id === userId);
-  
-        if (user) {
-          res.setHeader('Content-Type', 'application/json');
-          res.statusCode = 200;
-          res.end(JSON.stringify({ ...user, id: undefined }));
-        } else {
-          res.setHeader('Content-Type', 'application/json');
-          res.statusCode = 404;
-          res.end(JSON.stringify({ message: 'User not found' }));
-        }
-      }
+      handleGetUser(req, res);
     } else if (req.url === '/api/users' && req.method === 'POST') {
-      let body = '';
-    
-      req.on('data', (chunk) => {
-        body += chunk;
-      });
-    
-      req.on('end', () => {
-        const newUser = JSON.parse(body);
-    
-        if (!newUser.username || !newUser.age || !newUser.hobbies) {
-          res.setHeader('Content-Type', 'application/json');
-          res.statusCode = 400;
-          res.end(JSON.stringify({ message: 'Username, age, and hobbies are required' }));
-        } else {
-          const generatedId = uuidv4();
-          const createdUser = { id: generatedId, ...newUser };
-          users.push(createdUser);
-    
-          res.setHeader('Content-Type', 'application/json');
-          res.statusCode = 201;
-          res.end(JSON.stringify(createdUser));
-        }
-      });
+      handleCreateUser(req, res);
     } else if (req.url?.startsWith('/api/users/') && req.method === 'PUT') {
-      const userId = req.url.split('/')[3];
-      const userIndex = users.findIndex((u) => u.id === userId);
-    
-      if (!validateUuid(userId)) {
-        res.setHeader('Content-Type', 'application/json');
-        res.statusCode = 400;
-        res.end(JSON.stringify({ message: 'Invalid userId' }));
-      } else if (userIndex !== -1) {
-        let body = '';
-    
-        req.on('data', (chunk) => {
-          body += chunk;
-        });
-    
-        req.on('end', () => {
-          const updatedUser = JSON.parse(body);
-    
-          if (!updatedUser.username || !updatedUser.age) {
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 400;
-            res.end(JSON.stringify({ message: 'Username and age are required' }));
-          } else {
-            users[userIndex] = { ...users[userIndex], ...updatedUser, hobbies: updatedUser.hobbies || [] };
-    
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 200;
-            res.end(JSON.stringify(users[userIndex]));
-          }
-        });
-      } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.statusCode = 404;
-        res.end(JSON.stringify({ message: 'User not found' }));
-      }
+      handleUpdateUser(req, res);
     } else if (req.url?.startsWith('/api/users/') && req.method === 'DELETE') {
-      const userId = req.url.split('/')[3];
-    
-      if (!validateUuid(userId)) {
-        res.setHeader('Content-Type', 'application/json');
-        res.statusCode = 400;
-        res.end(JSON.stringify({ message: 'Invalid userId' }));
-      } else {
-        const userIndex = users.findIndex((u) => u.id === userId);
-    
-        if (userIndex !== -1) {
-          users.splice(userIndex, 1);
-          res.statusCode = 204;
-          res.end();
-        } else {
-          res.setHeader('Content-Type', 'application/json');
-          res.statusCode = 404;
-          res.end(JSON.stringify({ message: 'User not found' }));
-        }
-      }
+      handleDeleteUser(req, res);
     } else {
       res.setHeader('Content-Type', 'application/json');
       res.statusCode = 404;
@@ -131,5 +35,3 @@ const server = createServer((req: IncomingMessage, res: ServerResponse) => {
 server.listen(PORT, () => {
   console.log(`Server is running at ${PORT}/`);
 });
-
-
